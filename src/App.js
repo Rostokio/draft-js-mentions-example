e52df5461604617933c87d609356b0a454fc5808
Благушин Ростислав Фёдорович <Blagushin.R.F@sberbank.ru>
2021-04-27 12:10:14 +0300

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import {convertFromRaw, ContentState, EditorState} from 'draft-js';
+import {Editor} from 'react-draft-wysiwyg';
+import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 import './App.css';
+import {convertFromHTML} from "draft-convert";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = () => {
+    const ref = React.useRef();
+    const [editorState, setEditorState] = useState(EditorState.createEmpty());
+
+    const handleEditorChange = (state) => {
+        setEditorState(state);
+    }
+
+    const callBackend = (id) => {
+        fetch('http://localhost:8080/draft?id=' + id)
+            .then(result => result.text())
+            .then(result => setEditorState(EditorState.createWithContent(convertFromHTML(result))))
+            .catch(e => console.log(e));
+    }
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                Termsheets example
+            </header>
+            <Editor
+                editorState={editorState}
+                onEditorStateChange={handleEditorChange}
+                wrapperClassName="wrapper-class"
+                editorClassName="editor-class"
+                toolbarClassName="toolbar-class"
+                ref={ref}
+            />
+            <button onClick={() => {
+                callBackend("1")
+            }
+            }
+            >Вставить первый текст с бэка</button>
+
+            <button onClick={() => {
+                callBackend("2")
+            }
+            }
+            >Вставить второй текст с бэка</button>
+
+            <button onClick={() => {
+                callBackend("3")
+            }
+            }
+            >Вставить третий текст с бэка</button>
+        </div>
+
+    )
 }
-
 export default App;
